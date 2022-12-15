c60f0ee84dd5eb670978d4ab29e993f25a1e9643
liuyangjuncong20202570 <318135748@qq.com>
2022-12-15 16:00:28 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式
 import '@/styles/index.less'
+// 2. 引入组件样式
 import vant from 'vant'
 import 'vant/lib/index.css'
-// 2. 引入组件样式
-Vue.use(vant)
-
+// 引入amfe-flexible用于动态调整rem基准值
+import 'amfe-flexible'
 // 3. 注册你需要的组件
+Vue.use(vant)
 
 Vue.config.productionTip = false
 
