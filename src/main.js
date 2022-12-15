83b92b54500ff60317d16241fd21fd6b00d13527
liuyangjuncong20202570 <318135748@qq.com>
2022-12-15 15:29:58 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
+import vant from 'vant'
+import 'vant/lib/index.css'
+// 2. 引入组件样式
+Vue.use(vant)
+
+// 3. 注册你需要的组件
+
 Vue.config.productionTip = false
 
 new Vue({
